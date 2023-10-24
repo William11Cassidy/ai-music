57ecd4d41e29d69a431f99db4f7cb6d19f2b72ce
William11Cassidy <121349385+William11Cassidy@users.noreply.github.com>
2023-10-24 15:41:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-song1=""
-song2=""
+song1 = ""
+song2 = ""
 
-function preload(){
-song1=loadSound("One.mp3")
-song2=loadSound("harry.mp3")
+function preload() {
+    song1 = loadSound("One.mp3")
+    song2 = loadSound("harry.mp3")
 }
-function setup(){
-canvas=createCanvas(600,500)
-canvas.center()
-video=createCapture(VIDEO)
-video.hide()
+
+
+
+function setup() {
+    canvas = createCanvas(600, 500)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+    poseNet=ml5.poseNet(video,modelloaded)
+    poseNet.on("pose",gotposes)
 }
-function draw(){
-image(video,0,0,600,500)
+
+
+function draw() {
+    image(video, 0, 0, 600, 500)
+}
+
+function gotposes(result){
+if (result.length>0) {
+    console.log(result);
+}
+}
+
+function modelloaded(){
+    console.log("posenet is inisilized");
 }
